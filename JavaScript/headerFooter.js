3302e3124bd4d91006a7c4ad0fdd64f2530f1186
aayushlbef <aayush.gupta@study.lbef.edu.np>
2026-02-18 10:50:47 +0545

    ghgh

--- JavaScript/headerFooter.js
+++ JavaScript/headerFooter.js
@@ -7,13 +7,19 @@
 //   .then(data => document.getElementById("footer").innerHTML = data);
 
 
-// Determine base path dynamically
-const basePath = window.location.pathname.includes("/testing-path/")
-  ? "/testing-path/"
-  : "/";
+// Determine path depth dynamically to find the root folder
+const depth = window.location.pathname.split('/').filter(p => p !== "").length;
+// If hosted on GitHub Pages (e.g., /testing-path/...), the first segment is the repo name
+const isGitHubPages = window.location.pathname.includes("/testing-path/");
+const rootOffset = isGitHubPages ? depth - 1 : depth;
+
+let prefix = "";
+for (let i = 0; i < rootOffset; i++) {
+  prefix += "../";
+}
 
 // Load header
-fetch(`${basePath}HTML/navigationBar.html`)
+fetch(`${prefix}HTML/navigationBar.html`)
   .then(res => res.text())
   .then(data => {
     document.getElementById("header").innerHTML = data;
@@ -21,9 +27,10 @@ fetch(`${basePath}HTML/navigationBar.html`)
   .catch(err => console.error("Header error:", err));
 
 // Load footer
-fetch(`${basePath}HTML/footer.html`)
+fetch(`${prefix}HTML/footer.html`)
   .then(res => res.text())
   .then(data => {
     document.getElementById("footer").innerHTML = data;
   })
   .catch(err => console.error("Footer error:", err));
+
